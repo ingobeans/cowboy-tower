b20d91b2735c23b246822a2ec96881243239c0c1
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-12-20 01:17:13 +0100

    set up base game struct!

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,20 @@ use crate::{assets::Assets, utils::*};
 mod assets;
 mod utils;
 
-#[macroquad::main("cowboy tower")]
-async fn main() {
-    let assets = Assets::load();
-    loop {
+struct Game<'a> {
+    assets: &'a Assets,
+}
+impl<'a> Game<'a> {
+    fn new(assets: &'a Assets) -> Self {
+        Self { assets }
+    }
+    fn update(&mut self) {
         clear_background(Color::from_hex(0x3c9f9c));
         let (actual_screen_width, actual_screen_height) = screen_size();
         let scale_factor =
             (actual_screen_width / SCREEN_WIDTH).min(actual_screen_height / SCREEN_HEIGHT);
 
-        let t = &assets.cowboy.animations[0].get_at_time(0);
+        let t = &self.assets.cowboy.animations[0].get_at_time(0);
         draw_texture_ex(
             t,
             64.0,
@@ -25,6 +29,16 @@ async fn main() {
                 ..Default::default()
             },
         );
+    }
+}
+
+#[macroquad::main("cowboy tower")]
+async fn main() {
+    let assets = Assets::load();
+    let mut game = Game::new(&assets);
+
+    loop {
+        game.update();
         next_frame().await;
     }
 }
