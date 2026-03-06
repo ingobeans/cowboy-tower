4fe9eaf2c86d3aa8a2ae76b63fc74ac3f6137669
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-03-06 14:30:02 +0100

    added stairs! used for the (previosuly decorative) stairs on 0-boss

--- src/player/physics.rs
+++ src/player/physics.rs
@@ -49,8 +49,64 @@ pub fn update_physicsbody(
         tiles_y.push((ceil_g(tile_x), (new.y / 8.0).floor() - 1.0));
     }
 
+    fn handle_stair_walking(
+        tile: u16,
+        velocity: &mut Vec2,
+        new: &mut Vec2,
+        grounded: &mut bool,
+        pos: Vec2,
+        tx: f32,
+        ty: f32,
+    ) {
+        if velocity.y < 0.0 {
+            return;
+        }
+
+        // width is actually only 7.0 to leave 1px room on the apex of the stair,
+        // so that theres a tiny bit of margin til the next tile and you dont have weird collisions
+
+        let w = 7.0;
+        let h = 8.0;
+
+        if tile == 833 + 1 || tile == 832 + 1 {
+            let stair_pos = vec2(tx, ty) * 8.0
+                + if tile == 833 + 1 {
+                    vec2(1.0, 0.0)
+                } else {
+                    Vec2::ZERO
+                };
+
+            let pos = if tile == 833 + 1 {
+                pos
+            } else {
+                pos + vec2(8.0, 0.0)
+            };
+
+            // check if player is in stair x bound
+            let in_bound = if tile == 833 + 1 {
+                (stair_pos.x - 1.0..stair_pos.x + w).contains(&pos.x)
+            } else {
+                (stair_pos.x..stair_pos.x + w + 1.0).contains(&pos.x)
+            };
+
+            if in_bound {
+                // player x relative to stair origin
+                let player_stair_x = if tile == 833 + 1 {
+                    stair_pos.x - pos.x
+                } else {
+                    -(stair_pos.x - pos.x) - w
+                };
+                let target_height = player_stair_x / w * h;
+                new.y = stair_pos.y - (h + target_height.max(-8.0)).min(8.0);
+                velocity.y = 0.0;
+                *grounded = true;
+            }
+        }
+    }
+
     for (tx, ty) in tiles_y {
-        let mut tile = level.get_tile((tx) as i16, (ty) as i16)[1];
+        let tile_data = level.get_tile((tx) as i16, (ty) as i16);
+        let mut tile = tile_data[1];
         if !grounded && tile > 0 && DEATH_TILES.contains(&(tile - 1)) {
             if ty * 8.0 > pos.y {
                 touched_death_tile = Some(tile - 1);
@@ -74,6 +130,8 @@ pub fn update_physicsbody(
             new.y = c;
             velocity.y = 0.0;
             break;
+        } else {
+            handle_stair_walking(tile_data[3], velocity, &mut new, &mut grounded, pos, tx, ty);
         }
     }
     let mut tiles_x = vec![
@@ -115,6 +173,8 @@ pub fn update_physicsbody(
             new.x = c;
             velocity.x = 0.0;
             break;
+        } else {
+            handle_stair_walking(tile_data[3], velocity, &mut new, &mut grounded, pos, tx, ty);
         }
     }
     (
