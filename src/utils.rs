38d12276e362f0bd6632b291c2933d55eb720481
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-04-29 15:15:24 +0200

    move input part of utils to seperate file

--- src/utils.rs
+++ src/utils.rs
@@ -3,8 +3,11 @@ use macroquad::{miniquad::*, prelude::*};
 #[cfg(test)]
 use std::f32::consts::PI;
 use std::sync::LazyLock;
+
 mod debug;
+mod input;
 pub use debug::*;
+pub use input::*;
 
 pub const SCREEN_WIDTH: f32 = 256.0;
 pub const SCREEN_HEIGHT: f32 = 144.0;
@@ -28,95 +31,6 @@ pub fn create_camera(w: f32, h: f32) -> Camera2D {
         zoom: Vec2::new(1.0 / w * 2.0, 1.0 / h * 2.0),
         ..Default::default()
     }
-}
-
-pub fn is_pause_pressed(gamepad_engine: &mut Gamepads) -> bool {
-    is_key_pressed(KeyCode::Escape)
-        || gamepad_engine.all().any(|f| {
-            f.is_just_pressed(Button::LeftCenterCluster)
-                || f.is_just_pressed(Button::RightCenterCluster)
-        })
-}
-pub fn is_lasso_pressed(gamepad_engine: &mut Gamepads) -> bool {
-    is_mouse_button_pressed(MouseButton::Right)
-        || gamepad_engine.all().any(|f| {
-            f.is_just_pressed(Button::FrontLeftUpper) || f.is_just_pressed(Button::FrontLeftLower)
-        })
-}
-pub fn is_lasso_down(gamepad_engine: &mut Gamepads) -> bool {
-    is_mouse_button_down(MouseButton::Right)
-        || gamepad_engine.all().any(|f| {
-            f.is_currently_pressed(Button::FrontLeftUpper)
-                || f.is_currently_pressed(Button::FrontLeftLower)
-        })
-}
-pub fn is_shoot_down(gamepad_engine: &mut Gamepads) -> bool {
-    is_mouse_button_down(MouseButton::Left)
-        || gamepad_engine.all().any(|f| {
-            f.is_currently_pressed(Button::FrontRightUpper)
-                || f.is_currently_pressed(Button::FrontRightLower)
-        })
-}
-
-pub fn is_jump_pressed(gamepad_engine: &mut Gamepads) -> bool {
-    is_key_pressed(KeyCode::Space)
-        || gamepad_engine.all().any(|f| {
-            f.is_just_pressed(Button::ActionRight) || f.is_just_pressed(Button::ActionDown)
-        })
-}
-pub fn is_jump_down(gamepad_engine: &mut Gamepads) -> bool {
-    is_key_down(KeyCode::Space)
-        || gamepad_engine.all().any(|f| {
-            f.is_currently_pressed(Button::ActionRight)
-                || f.is_currently_pressed(Button::ActionDown)
-        })
-}
-pub fn is_interact_pressed(gamepad_engine: &mut Gamepads) -> bool {
-    is_key_pressed(KeyCode::E)
-        || gamepad_engine.all().any(|f| {
-            f.is_just_pressed(Button::ActionRight) || f.is_just_pressed(Button::ActionDown)
-        })
-}
-
-pub fn get_input_axis(gamepad_engine: &mut Gamepads) -> Vec2 {
-    let mut i = Vec2::ZERO;
-
-    for controller in gamepad_engine.all() {
-        let axis: Vec2 = controller.left_stick().into();
-        if axis.length() <= 0.2 {
-            // check d-pad
-            let left = controller.is_currently_pressed(Button::DPadLeft);
-            let right = controller.is_currently_pressed(Button::DPadRight);
-            let up = controller.is_currently_pressed(Button::DPadUp);
-            let down = controller.is_currently_pressed(Button::DPadDown);
-            if left || right || up || down {
-                let horizontal = 0.0 - if left { 1.0 } else { 0.0 } + if right { 1.0 } else { 0.0 };
-                let vertical = 0.0 + if down { 1.0 } else { 0.0 } - if up { 1.0 } else { 0.0 };
-                return vec2(horizontal, vertical);
-            }
-        } else {
-            // cap axis values to -1 or 1, disallow decimal values
-            let horizontal =
-                0.0 - if axis.x < 0.0 { 1.0 } else { 0.0 } + if axis.x > 0.0 { 1.0 } else { 0.0 };
-            let vertical =
-                0.0 + if axis.y < 0.0 { 1.0 } else { 0.0 } - if axis.y > 0.0 { 1.0 } else { 0.0 };
-            return vec2(horizontal, vertical);
-        }
-    }
-
-    if is_key_down(KeyCode::A) {
-        i.x -= 1.0;
-    }
-    if is_key_down(KeyCode::D) {
-        i.x += 1.0;
-    }
-    if is_key_down(KeyCode::W) {
-        i.y -= 1.0;
-    }
-    if is_key_down(KeyCode::S) {
-        i.y += 1.0;
-    }
-    i
 }
 
 pub static SKY_MATERIAL: LazyLock<Material> = LazyLock::new(|| {
